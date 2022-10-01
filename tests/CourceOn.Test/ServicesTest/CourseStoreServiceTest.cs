bd04d906f359c1ed4382deb6c4a57ecfe6b9838b
Ruan Rodrigues <ruansjn@gmail.com>
2022-09-30 23:57:29 -0300

    feat: implementando novos testes com TDD

--- tests/CourceOn.Test/ServicesTest/CourseStoreServiceTest.cs
+++ tests/CourceOn.Test/ServicesTest/CourseStoreServiceTest.cs
@@ -1,5 +1,7 @@
 ﻿using CourseOn.Domain.Enum;
 using CourseOn.Domain.Model;
+using CourseOn.Test.Builders;
+using CourseOn.Test.Util;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -12,28 +14,41 @@ namespace CourseOn.Test.ServicesTest
     public class CourseStoreServiceTest
     {
         private readonly Mock<ICourseRepository> _courseRepositoryMock;
+        private readonly CourseStoreService _courseStoreService;
+        private CourseDto _courseDto;
 
         public CourseStoreServiceTest()
-        {
-            _courseRepositoryMock = new Mock<ICourseRepository>();
-        }
-
-        [Fact]
-        public void MustAddCourse()
         {
             var courseDto = new CourseDto
             {
                 Name = "Ruan",
                 Workload = 10,
                 Price = 100.00,
-                TargetAudience = 1
+                TargetAudience = TargetAudience.Entrepreneur
             };
 
-            var courseStoreService = new CourseStoreService(_courseRepositoryMock.Object);
+            _courseDto = courseDto;
+            _courseRepositoryMock = new Mock<ICourseRepository>();
+            _courseStoreService = new CourseStoreService(_courseRepositoryMock.Object);
+        }
 
-            courseStoreService.Store(courseDto);
+        [Fact]
+        public void MustAddCourse()
+        {
+            _courseStoreService.Store(_courseDto);
 
-            _courseRepositoryMock.Verify(r => r.Add(It.Is<Course>(c => c.Name == courseDto.Name && c.Price == courseDto.Price)));
+            _courseRepositoryMock.Verify(r => r.Add(It.Is<Course>(c => c.Name == _courseDto.Name && c.Price == _courseDto.Price)));
+        }
+
+        [Fact]
+        public void MustNotAddCourseAlreadySavedWithSameName()
+        {
+            var courseAlreadySaved = CourseBuilder.New().WithName(_courseDto.Name).Builder();
+
+            _courseRepositoryMock.Setup(c => c.GetByName(_courseDto.Name)).Returns(courseAlreadySaved);
+
+            Assert.Throws<ArgumentException>(() => _courseStoreService.Store(_courseDto)).WithMessage("Course Already Saved.");
+            
         }
     }
 
@@ -41,13 +56,18 @@ namespace CourseOn.Test.ServicesTest
     {
         private readonly ICourseRepository _courseRepository;
 
-        public CourseStoreService(ICourseRepository @object)
+        public CourseStoreService(ICourseRepository courseRepository)
         {
-            _courseRepository = @object;
+            _courseRepository = courseRepository;
         }
 
         public void Store(CourseDto courseDto)
         {
+            var courseAlreadySaved = _courseRepository.GetByName(courseDto.Name);
+
+            if (courseAlreadySaved != null)
+                throw new ArgumentException("Course Already Saved.");
+
             var course = new Course(courseDto.Name, courseDto.Workload, (TargetAudience)courseDto.TargetAudience, courseDto.Price);
 
             _courseRepository.Add(course);
@@ -59,12 +79,13 @@ namespace CourseOn.Test.ServicesTest
         public string Name { get; set; }
         public int Workload { get; set; }
         public double Price { get; set; }
-        public int TargetAudience { get; set; }
+        public TargetAudience TargetAudience { get; set; }
     }
 
     public interface ICourseRepository
     {
         void Add(Course course);
         void Update(Course course);
+        Course GetByName(string name);
     }
 }
